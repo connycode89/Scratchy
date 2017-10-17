658ef737e843fc2b00d599c38961251bb420eb2a
Donovan <cdonovan@TYCOFS.COM>
2017-10-17 11:36:40 +0100

    Finished full algorithm

--- kMeans Clustering.py
+++ kMeans Clustering.py
@@ -12,7 +12,7 @@ import numpy as np
 # input data here in the form of a 2D numpy array
 # the array should be entirely numeric & have rows as observations & columns as features
 # data = ?
-data = np.array([[1,2,3],[4,5,6],[7,8,9],[10,11,12]])
+data = np.array([[1,1,1],[1,1,2],[8,8,9],[7,8,9]])
 
 def euclidean(pt1, pt2):
     """ Euclidean distance between 2 points, pt1 and pt2
@@ -43,12 +43,33 @@ def dists_to_centroids(data, centroids):
     dists = np.array(list2).T
     return dists
 
-aaa = random_centroids(data,3)        
-dists_to_centroids(data,aaa)
+def iterate_once(data, prev_clusts, cents):
+    list_cents = []
+    for num in range(len(cents)):
+        clust_num = data[prev_clusts==num]
+        if len(clust_num)!=0:
+            cent2 = np.mean(clust_num,axis=0)
+            list_cents.append(list(cent2))
+        else:
+            list_cents.append(list(random_centroids(data,1)[0]))
+    new_cents = np.array(list_cents)
+    return new_cents
+
+def algorithm(data,k):
+    centroids = random_centroids(data,k)        
+    dists = dists_to_centroids(data,centroids)
+    clusts = np.argmin(dists, axis=1)
+    while True:    
+        centroids = iterate_once(data, clusts, centroids)
+        dists = dists_to_centroids(data,centroids)
+        clusts2 = np.argmin(dists, axis=1)
+        if np.all(clusts==clusts2):
+            break
+    wss = np.sum(np.min(dists_to_centroids(data,centroids), axis=1))
+    return clusts2, wss
+
+k = 2
+algorithm(data,k)
 
 # to add: rest of algorithm
 # plotting
-
-
-        
-    
